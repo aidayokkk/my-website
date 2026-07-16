4c4da1c4e162e0a50759929efdb4ddaa40357d31
Nishikawa aika <n223t120@st.kyoto-seika.ac.jp>
2026-07-16 17:08:42 +0900

    わー

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/homepage/',
+  base: '/my-website/',
   build: {
     outDir: 'docs',
     rollupOptions: {
